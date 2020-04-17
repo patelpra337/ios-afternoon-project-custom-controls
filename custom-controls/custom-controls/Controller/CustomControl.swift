cd3e171b17b501cdb6b37ed9787264d10792f96e
Pravin Patel <patelpra337@gmail.com>
2020-04-16 22:27:33 -0500

    Update Storyboard and CustomControl file

--- custom-controls/custom-controls/Controller/CustomControl.swift
+++ custom-controls/custom-controls/Controller/CustomControl.swift
@@ -44,14 +44,46 @@ class CustomControl: UIControl {
             } else {
                 label.textColor = componentInactiveColor
             }
-            
         }
-        
-        
     }
     
+    override var intrinsicContentSize: CGSize {
+      let componentsWidth = CGFloat(componentCount) * componentDimension
+      let componentsSpacing = CGFloat(componentCount + 1) * 8.0
+      let width = componentsWidth + componentsSpacing
+      return CGSize(width: width, height: componentDimension)
+    }
     
+    private func updateValue(at touch: UITouch) {
+        _ = touch.location(in: self)
+    }
     
+    override func beginTracking(_ touch: UITouch, with event: UIEvent?) -> Bool {
+        updateValue(at: touch)
+        return true
+    }
     
+    override func continueTracking(_ touch: UITouch, with event: UIEvent?) -> Bool {
+        <#code#>
+    }
     
+    override func endTracking(_ touch: UITouch?, with event: UIEvent?) {
+        defer {
+            super.endTracking(touch, with: event)
+        }
+        guard let touch = touch else { return }
+        
+        let touchPoint = touch.location(in: self)
+        if bounds.contains(touchPoint) {
+            sendActions(for: [.touchUpInside, .valueChanged])
+            updateValue(at: touch)
+        } else {
+            sendActions(for: [.touchCancel])
+        }
+    }
+    
+    override func cancelTracking(with event: UIEvent?) {
+    sendActions(for: [.touchCancel])
+        
+    }    
 }
