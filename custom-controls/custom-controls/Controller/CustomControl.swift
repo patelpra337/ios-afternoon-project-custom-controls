4f24a31337c3278668951a52101138f6756bd732
Pravin Patel <patelpra337@gmail.com>
2020-04-16 19:26:42 -0500

    Create new project and organize project

---
+++ custom-controls/custom-controls/Controller/CustomControl.swift
@@ -0,0 +1,19 @@
+//
+//  CustomControl.swift
+//  custom-controls
+//
+//  Created by patelpra on 4/16/20.
+//  Copyright © 2020 Crus Technologies. All rights reserved.
+//
+
+import UIKit
+
+class CustomControl: UIControl {
+    
+    
+    
+    
+    
+    
+    
+}
---
+++ custom-controls/custom-controls/View Controller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  custom-controls
+//
+//  Created by patelpra on 4/16/20.
+//  Copyright © 2020 Crus Technologies. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
